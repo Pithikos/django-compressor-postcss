fcbf7c8db03e355150fd347dfc0d0ecb20e91347
Pithikos <manossef@hotmail.com>
2017-01-18 12:39:09 +0000

    Update code to a working condition with Django and django-compressor

--- tests/testenv_setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Setup a small virtual environment for testing
-virtualenv --clear testenv
-source testenv/bin/activate
-mkdir testenv/tmp
-cd testenv/tmp
-shopt -s extglob
-cp -r ../../../!(tests) .
